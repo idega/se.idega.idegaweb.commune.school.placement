88f521008ae1d5b14ec6919b78d1dc071c49bc27
Thorhallur Helgason <laddi@idega.com>
2004-01-07 13:41:24 +0000

    Changed to use a double dropdown menu when selecting year and group.

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolAdminOverview.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolAdminOverview.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import javax.ejb.CreateException;
@@ -61,6 +62,7 @@ import com.idega.presentation.ui.Form;
 import com.idega.presentation.ui.HiddenInput;
 import com.idega.presentation.ui.PrintButton;
 import com.idega.presentation.ui.RadioButton;
+import com.idega.presentation.ui.SelectDropdownDouble;
 import com.idega.presentation.ui.SubmitButton;
 import com.idega.presentation.ui.TextArea;
 import com.idega.presentation.ui.TextInput;
@@ -774,7 +776,7 @@ public class SchoolAdminOverview extends CommuneBlock {
 			table.add(getSmallErrorText(localize("school.replace_date", "Replace date") + ":" + Text.NON_BREAKING_SPACE + Text.NON_BREAKING_SPACE + Text.NON_BREAKING_SPACE), 1, row);
 		table.add(input, 1, row++);
 
-		table.add(getNavigationTable(iwc, localize("school.replace_to", "Replace to") + ":", true), 1, row++);
+		table.add(getNavigationTable(iwc, localize("school.replace_to", "Replace to") + ":"), 1, row++);
 
 		//if (_protocol)
 		table.add(getSmallHeader(localize("school.replace_reason", "Replace reason") + ":"), 1, row);
@@ -891,7 +893,7 @@ public class SchoolAdminOverview extends CommuneBlock {
 
 		table.add(getSmallHeader(localize("school.move_group_info", "Select the new group for the student and click 'Move'.")), 1, row++);
 
-		table.add(getNavigationTable(iwc, localize("school.move_to", "Move to") + ":", false), 1, row++);
+		table.add(getNavigationTable(iwc, localize("school.move_to", "Move to") + ":"), 1, row++);
 
 		SubmitButton move = (SubmitButton) getStyledInterface(new SubmitButton(localize("school.move", "Move"), PARAMETER_ACTION, String.valueOf(ACTION_MOVE_GROUP)));
 		move.setValueOnClick(PARAMETER_METHOD, "-1");
@@ -1530,22 +1532,46 @@ public class SchoolAdminOverview extends CommuneBlock {
 	}
 	
 	
-	protected Table getNavigationTable(IWContext iwc, String heading, boolean setToSubmit) throws RemoteException {
-		Table table = new Table(7, 1);
+	protected Table getNavigationTable(IWContext iwc, String heading) throws RemoteException {
+		Table table = new Table(4, 1);
 		table.setCellpadding(0);
 		table.setCellspacing(0);
 		table.setWidth(2, "8");
-		table.setWidth(5, "8");
+		//table.setWidth(5, "8");
 
 		table.add(getSmallHeader(heading), 1, 1);
-		table.add(getSmallHeader(localize("school.year", "Year") + ":" + Text.NON_BREAKING_SPACE), 3, 1);
-		table.add(getSchoolYears(iwc), 4, 1);
+		table.add(getSmallHeader(localize("school.year_class", "Year/Class") + ":" + Text.NON_BREAKING_SPACE), 3, 1);
+		table.add(getDropdown(iwc), 4, 1);
+		/*table.add(getSchoolYears(iwc), 4, 1);
 		table.add(getSmallHeader(localize("school.class", "Class") + ":" + Text.NON_BREAKING_SPACE), 6, 1);
-		table.add(getSchoolClasses(iwc, setToSubmit), 7, 1);
+		table.add(getSchoolClasses(iwc, setToSubmit), 7, 1);*/
 
 		return table;
 	}
 
+	private SelectDropdownDouble getDropdown(IWContext iwc) throws RemoteException {
+		SelectDropdownDouble dropdown = new SelectDropdownDouble(getSchoolCommuneSession(iwc).getParameterSchoolYearID(), getSchoolCommuneSession(iwc).getParameterSchoolClassID());
+
+		try {
+			if (getSchoolCommuneSession(iwc).getSchoolID() != -1) {
+				Collection years = getSchoolCommuneBusiness(iwc).getSchoolBusiness().findAllSchoolYearsInSchool(getSchoolCommuneSession(iwc).getSchoolID());
+				if (!years.isEmpty()) {
+					Map yearGroupMap = getSchoolCommuneBusiness(iwc).getYearClassMap(years, _schoolID, getSchoolCommuneSession(iwc).getSchoolSeasonID(), null);
+					Iterator iter = yearGroupMap.keySet().iterator();
+					while (iter.hasNext()) {
+						SchoolYear year = (SchoolYear) iter.next();
+						dropdown.addMenuElement(year.getPrimaryKey().toString(), year.getSchoolYearName(), (Map) yearGroupMap.get(year));
+					}
+				}
+			}
+		}
+		catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		return dropdown;
+	}
+	
 	private void reject(IWContext iwc) throws RemoteException {
 		String messageHeader = localize("school.reject_message_header", "School choice rejected.");
 		String messageBody = iwc.getParameter(PARAMETER_REJECT_MESSAGE);

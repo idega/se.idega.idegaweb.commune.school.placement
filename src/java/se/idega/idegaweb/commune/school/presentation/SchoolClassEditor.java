6cfd8c079b2f9115868c82b1d21a15438fecc8fe
Pall Helgason <palli@idega.com>
2006-01-20 13:45:49 +0000

    Removed unused code

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolClassEditor.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolClassEditor.java
@@ -1472,7 +1472,7 @@ public class SchoolClassEditor extends SchoolAccountingCommuneBlock {
 		return table;
 	}
 
-	private Link getListOfCoordinatesXLSLink(Class classToUse, Image image) throws RemoteException {
+	private Link getListOfCoordinatesXLSLink(Class classToUse, Image image) {
 		Link link = new Link(image);
 		link.setWindow(getFileWindow());
 		link.addParameter(MediaWritable.PRM_WRITABLE_CLASS, IWMainApplication.getEncryptedClassName(classToUse));

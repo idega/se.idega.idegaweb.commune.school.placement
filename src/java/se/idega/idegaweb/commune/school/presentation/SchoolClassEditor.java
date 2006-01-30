531a332e28148cddad8e8927c7c41d733ec404e0
Pall Helgason <palli@idega.com>
2006-01-30 16:23:55 +0000

    Removed unthrown exception

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolClassEditor.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolClassEditor.java
@@ -1483,7 +1483,7 @@ public class SchoolClassEditor extends SchoolAccountingCommuneBlock {
 		return table;
 	}
 
-	private Link getListOfCoordinatesXLSLink(Class classToUse, Image image) throws RemoteException {
+	private Link getListOfCoordinatesXLSLink(Class classToUse, Image image) {
 		Link link = new Link(image);
 		link.setWindow(getFileWindow());
 		link.addParameter(MediaWritable.PRM_WRITABLE_CLASS, IWMainApplication.getEncryptedClassName(classToUse));
